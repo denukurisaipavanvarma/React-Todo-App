45b6f5a405b60f7eadd447042aa8d1af57f3eb4e
denukurisaipavanvarma <saipavanvarmadendukuri@gmail.com>
2025-11-30 12:12:27 +0530

    style: set filter button colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from "react";
 import "./css/main.css";
 import DisplayTodos from "./components/DisplayTodos";
 import Todos from "./components/Todos";
-
 import { motion } from "framer-motion";
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
       >
         Todo App
       </motion.h1>
+
       <motion.div
         initial={{ y: 1000 }}
         animate={{ y: 0 }}
